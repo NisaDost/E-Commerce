552006c236ca1010191eb514eb1b8942df145d9c
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-04-22 16:59:30 +0300

    denedim...

--- E-commerce/obj/Release/net8.0/E-commerce.AssemblyInfo.cs
+++ E-commerce/obj/Release/net8.0/E-commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c88fd3b3a16ca74f6aa9f5ec263cbc4830c637")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
