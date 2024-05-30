90eb5625399a003d85fcf750db8f15cd17e69db7
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-05-30 18:45:23 +0300

    auth trials 2

--- ECOMMERCE2/Controllers/LoginController.cs
+++ ECOMMERCE2/Controllers/LoginController.cs
@@ -1,15 +1,44 @@
 ﻿using ECOMMERCE2.Data.Model;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ECOMMERCE2.Models;
+using ECOMMERCE2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECOMMERCE2.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly DBContext _context;
         public IActionResult Index()
         {
-            var role = new User().Role;
-            ViewBag.Role = role;
-            return RedirectToAction("Index", "Home", role);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginViewModel loginViewModel)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginViewModel.Username && u.Password == loginViewModel.Password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, user.Role),
+                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString())
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties { };
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password!";
+
+            return View("Index");
         }
     }
 }
--- ECOMMERCE2/Helper/UserHelper.cs
+++ ECOMMERCE2/Helper/UserHelper.cs
@@ -1,6 +1,19 @@
-﻿namespace ECOMMERCE2.Helper
+﻿using System.Security.Claims;
+
+namespace ECOMMERCE2.Helper
 {
     public class UserHelper
     {
+        public static int? GetUserId(ClaimsPrincipal user)
+        {
+            string userIdValue = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (int.TryParse(userIdValue, out int userId))
+            {
+                return userId;
+            }
+
+            return null;
+        }
     }
 }
