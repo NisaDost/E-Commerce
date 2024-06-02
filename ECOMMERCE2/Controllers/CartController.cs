be17b5d918cd2913f622e9976f7c59d102e028c1
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-06-02 16:20:17 +0300

    halfway to billing
    Co-Authored-By: Yiğit Aktüre <115189586+YigitAkture@users.noreply.github.com>

--- ECOMMERCE2/Controllers/CartController.cs
+++ ECOMMERCE2/Controllers/CartController.cs
@@ -1,16 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECOMMERCE2.Data;
+using ECOMMERCE2.Data.Model;
+using ECOMMERCE2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECOMMERCE2.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DBContext _context;
+        public CartController(DBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Checkout()
         {
+            var billingAdress = _context.BillingAddresses.ToList();
+            ViewBag.billingAdress = billingAdress;
+
+            var billingCard = _context.BillingCard.ToList();
+            ViewBag.billingCard = billingCard;
             return View();
+        }
+        [HttpPost]
+        public IActionResult SaveAddress(BillingViewModel billingViewModel)
+        {
+            if (billingViewModel.SaveAddress)
+            {
+                var billingAddress = new BillingAddress
+                {
+                    FirstName = billingViewModel.FirstName,
+                    LastName = billingViewModel.LastName,
+                    Email = billingViewModel.Email,
+                    Mobile = billingViewModel.Mobile,
+                    Country = billingViewModel.Country,
+                    City = billingViewModel.City,
+                    Address = billingViewModel.Address
+                };
+                _context.BillingAddresses.Add(billingAddress);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAddress(int id)
+        {
+            var billingAddress = _context.BillingAddresses.Find(id);
+            return View(billingAddress);
+        }
+
+        [HttpPost]
+        public IActionResult SaveCard(BillingViewModel billingViewModel)
+        {
+            if (billingViewModel.SaveCard)
+            {
+                var billingCard = new BillingCard
+                {
+                    CreditCardNumber = billingViewModel.CreditCardNumber,
+                    CardHolderName = billingViewModel.CardHolderName,
+                    ExpiryDateMonth = billingViewModel.ExpiryDateMonth,
+                    ExpiryDateYear = billingViewModel.ExpiryDateYear,
+                    CVV = billingViewModel.CVV
+                };
+                _context.BillingCard.Add(billingCard);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetCard(int id)
+        {
+            var billingCard = _context.BillingCard.Find(id);
+            return View(billingCard);
         }
     }
 }
---
+++ ECOMMERCE2/Models/BillingViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ECOMMERCE2.Data.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECOMMERCE2.Models
+{
+    public class BillingViewModel
+    {
+        //address
+        public int BillingAddressId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public bool SaveAddress { get; set; }
+
+
+        //card
+
+        public int BillingCardId { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public int ExpiryDateMonth { get; set; }
+        public int ExpiryDateYear { get; set; }
+        public string CVV { get; set; }
+        public bool SaveCard { get; set; }
+
+    }
+}
