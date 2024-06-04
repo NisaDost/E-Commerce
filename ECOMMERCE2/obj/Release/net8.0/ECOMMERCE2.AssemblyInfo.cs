708bd8549f283bfffa38c569299eb060df9cd809
Yigit Akture <220706301@st.maltepe.edu.tr>
2024-06-04 22:13:20 +0300

    ??
    Co-Authored-By: Nisa Dost <76488638+NisaDost@users.noreply.github.com>

--- ECOMMERCE2/obj/Release/net8.0/ECOMMERCE2.AssemblyInfo.cs
+++ ECOMMERCE2/obj/Release/net8.0/ECOMMERCE2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECOMMERCE2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10532ac6ea32af81b1d9d7d91fde8cf233f8b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECOMMERCE2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECOMMERCE2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
