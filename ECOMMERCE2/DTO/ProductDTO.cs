5d946af484f47e214a1d98baaf9e1347e765045a
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-06-05 16:24:28 +0300

    product order by price error not fixed.
    Co-Authored-By: Yiğit Aktüre <115189586+YigitAkture@users.noreply.github.com>

---
+++ ECOMMERCE2/DTO/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ECOMMERCE2.DTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public string Picture { get; set; }
+    }
+
+}
